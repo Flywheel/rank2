e2af980953d8d56e8ceb3b0928d067c0271726c9
Craig Simon <cls@rkey.com>
2024-08-04 19:42:23 -0400

    Preparing for inmemorydbService

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,17 @@ import { isDevMode } from '@angular/core';
 async function prepareApp() {
   try {
     if (isDevMode()) {
-      const { worker: mockData } = await import('./mocks/browser');
-      return mockData.start();
+      const { worker: mockDataWorker } = await import('./mocks/browser');
+      console.log('Starting Mock Service Worker');
+      return mockDataWorker.start();
+    } else {
+      console.log('No data service yet provided');
     }
   } catch (error) {
     console.error('Failed to start Mock Service Worker:', error);
+    // const { module: importProvidersFrom } = await import('@angular/core')
+    // const { module: InMemoryWebApiModule } = await import('angular-in-memory-web-api');
+    // const c = {...appConfig.providers, InMemoryWebApiModule.forRoot(importProvidersFrom('./mocks/handlers'))};
   }
   return Promise.resolve();
 }
