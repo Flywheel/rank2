e18088bad7de32946334fbd075ef53246c518265
Craig Simon <cls@rkey.com>
2024-09-25 13:25:45 -0400

    retrying environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: true,
   HOST_DOMAIN: 'https://mh5.netlify.app',
-  azureSasToken: process.env['AZURE_SAS_TOKEN'], // Netlify will replace this with the actual value
-  azureStorageAccount: process.env['AZURE_STORAGE_ACCOUNT'], // Netlify will replace this with the actual value
-  emailEncryptionKey: process.env['EMAIL_ENCRYPTION_KEY'], // Netlify will replace this with the actual value
-  betaTestAuthKey: process.env['BETA_AUTH_KEY'], // Netlify will replace this with the actual value
+  betaTestAuthKey: '',
   ianConfig: {
     hideAuthorComponent: true,
     hideLogs: true,
